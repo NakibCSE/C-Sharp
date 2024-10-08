7bd62cbcd13ca79877ba3ee4b80e1f92a313528b
Nakib <nakkibcse@gmail.com>
2024-10-08 21:26:01 +0600

    Practice string builder

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Text;
+
+StringBuilder stringBuilder = new StringBuilder("I am from Chittagong");
+stringBuilder.Append(" and I love programming");
+stringBuilder = stringBuilder.Replace('a', 'x');
+stringBuilder = stringBuilder.Insert(10, "wwww");
+stringBuilder = stringBuilder.Remove(15, 5);
+for (int i = 0; i < stringBuilder.Length; i++)
+{
+    stringBuilder[i] = 'M';
+}
+Console.WriteLine(stringBuilder);
