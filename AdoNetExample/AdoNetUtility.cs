2807a008511597bc076d96efd348ffb7f5896bc9
Nakib <nakkibcse@gmail.com>
2024-10-24 13:18:21 +0600

    Adodotnet practice

---
+++ AdoNetExample/AdoNetUtility.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetExample
+{
+    public class AdoNetUtility
+    {
+        private readonly string __connectionString;
+        public AdoNetUtility(string connectionString)
+        {
+            __connectionString = connectionString;
+        }
+    
+        public void RunSql(string sql, Dictionary<string, object> parameters)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(__connectionString);
+            using SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+
+            foreach (var parameter in parameters)
+            {
+                sqlCommand.Parameters.Add(new SqlParameter(parameter.Key, parameter.Value));
+            }
+
+            if(sqlConnection.State != System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Open();
+            }
+            int rowEffected = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Dispose();
+            sqlCommand.Dispose(); 
+        }
+        public List<Dictionary<string, object>> GetData(string sql, Dictionary<string, object>parameters)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(__connectionString);
+            using SqlCommand sqlCommand = new SqlCommand( sql, sqlConnection);
+
+            foreach(var parameter in parameters)
+            {
+                sqlCommand.Parameters.Add(new SqlParameter(parameter.Key, parameter.Value));
+            }
+            if(sqlConnection.State != System.Data.ConnectionState.Open)
+            { sqlConnection.Open(); }
+
+            List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            while(sqlDataReader.Read())
+            {
+                Dictionary<string, object> row = new Dictionary<string, object>();
+                for(int i = 0; i < sqlDataReader.FieldCount; i++)
+                {
+                    string columnName = sqlDataReader.GetName(i);
+                    object value = sqlDataReader.GetValue(i);
+
+                    row.Add(columnName, value);
+                }
+                result.Add(row);
+            }
+            return result;
+        }
+    }
+}
---
+++ AdoNetExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using AdoNetExample;
+string __connectionString = "Server=DESKTOP-O70O0UG\\SQLEXPRESS;Database=CSharpB18;User Id=CSharpB18;Password=12345; Trust Server Certificate=True";
+AdoNetUtility adoNetUtility = new AdoNetUtility(__connectionString);
+var sql = "insert into Employee(Name, DateOfBirth, IsMarried) values(@name, @dateofbirth, @ismarried)";
+
+Dictionary<string, object> keyValuePairs = new Dictionary<string, object>();
+keyValuePairs.Add("@name", "Khan Mr.");
+keyValuePairs.Add("@dateofbirth", new DateTime(2000, 10, 1));
+keyValuePairs.Add("@ismarried", true);
+
+adoNetUtility.RunSql(sql, keyValuePairs);
+
+var getData = "select *from employee where id < @ID";
+Dictionary<string, object> parameters = new Dictionary<string, object>();
+parameters.Add("@ID", 10);
+
+var result = adoNetUtility.GetData(getData, parameters);
+
+foreach (var row in result)
+{
+    foreach (var key in row.Keys)
+    {
+        Console.Write(row[key]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
