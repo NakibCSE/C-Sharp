87636a614a13788a65a8d3398452864fcec5c86f
Nakib <nakkibcse@gmail.com>
2024-11-23 09:23:23 +0600

    More practice on directory

--- FileAndDirectory/Program.cs
+++ FileAndDirectory/Program.cs
@@ -1,11 +1,11 @@
-﻿//File.Create("Demo.txt");
-//File.WriteAllText("demo.txt", "Hello World");\
-//File.WriteAllLines("demo.txt", ["Dhaka", "Chittagong", "Sylhet"]);
+﻿using System.Text;
+File.Create("Demo.txt");
+File.WriteAllText("demo.txt", "Hello World");\
+File.WriteAllLines("demo.txt", ["Dhaka", "Chittagong", "Sylhet"]);
 
-using System.Text;
 
 FileInfo fileInfo = new FileInfo("demo.txt");
-if(fileInfo.Exists)
+if (fileInfo.Exists)
 {
     fileInfo.Delete();
 }
@@ -13,3 +13,39 @@ if(fileInfo.Exists)
 using FileStream fileStream = File.Open("Demo.txt", FileMode.OpenOrCreate);
 string text = "Hello World";
 fileStream.Write(Encoding.UTF8.GetBytes(text));
+
+
+//Directory
+Directory.CreateDirectory("Videos/Training");
+
+string CurrentDirectory = Directory.GetCurrentDirectory();
+Console.WriteLine(CurrentDirectory);
+Console.WriteLine();
+
+Directory.SetCurrentDirectory(Path.Combine(CurrentDirectory, "Videos", "Training"));
+CurrentDirectory = Directory.GetCurrentDirectory();
+Console.WriteLine(CurrentDirectory);
+Console.WriteLine();
+
+File.WriteAllText("Sample.txt", "Hello World");
+
+DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(CurrentDirectory, "Videos", "Training"));
+if (directoryInfo.Exists)
+{
+    Directory.Delete(Path.Combine(CurrentDirectory, "Videos", "Training"), true);
+}
+
+Directory.Delete(Path.Combine(CurrentDirectory, "Videos", "Training"), true);
+
+var dir2 = new DirectoryInfo(CurrentDirectory);
+var dir = dir2.EnumerateDirectories();
+foreach (var d in dir)
+{
+    Console.WriteLine(d.FullName);
+}
+
+var files = dir2.EnumerateFiles();
+foreach(var x in files)
+{
+    Console.WriteLine(x.FullName);
+}
