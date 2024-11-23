2ecdc40e512a8ff5e32270bf111cc73314ffdfcb
Nakib <nakkibcse@gmail.com>
2024-11-23 23:21:37 +0600

    a test project added

---
+++ TestProj/Fun1/Fun1/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fun1
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { optionsBuilder.UseSqlServer("Data Source=DESKTOP-O70O0UG\\SQLEXPRESS;Initial Catalog=CSharpB18;User ID=csharpb18;Password=123456;TrustServerCertificate=True;"); }
+    }
+}
---
+++ TestProj/Fun1/Fun1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Fun1;
+using System;
+using System.IO;
+
+using var context = new ApplicationDBContext();
+var lines = File.ReadAllLines("Data.txt");
+foreach (var line in lines)
+{
+    var data = line.Split(' ');
+    var student = new Student
+    {
+        Name = data[0],
+        Age = int.Parse(data[1]),
+        CGPA = double.Parse(data[2])
+    };
+    context.Students.Add(student);
+}
+context.SaveChanges();
