4901601669d4e0d2ca789b99162fc8e00d72eb2e
Nakib <nakkibcse@gmail.com>
2024-11-23 08:16:40 +0600

    file and directory practice

--- FileAndDirectory/Program.cs
+++ FileAndDirectory/Program.cs
@@ -1,3 +1,15 @@
 ﻿//File.Create("Demo.txt");
 //File.WriteAllText("demo.txt", "Hello World");\
-File.WriteAllLines("demo.txt", ["Dhaka", "Chittagong", "Sylhet"]);
+//File.WriteAllLines("demo.txt", ["Dhaka", "Chittagong", "Sylhet"]);
+
+using System.Text;
+
+FileInfo fileInfo = new FileInfo("demo.txt");
+if(fileInfo.Exists)
+{
+    fileInfo.Delete();
+}
+
+using FileStream fileStream = File.Open("Demo.txt", FileMode.OpenOrCreate);
+string text = "Hello World";
+fileStream.Write(Encoding.UTF8.GetBytes(text));
